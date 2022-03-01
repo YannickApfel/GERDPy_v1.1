3a17bcd15a8a7ec9c7c5cee3627890092cbc2da7
YannickApfel <yannick.apfel93@gmail.com>
2022-03-01 18:28:11 +0100

    GERDPy_v2.0_final_4
    minor changes

--- R_th_he.py
+++ R_th_he.py
@@ -3,16 +3,16 @@
 
     R_th_he:
 
-        - analytisch ermittelter therm. Widerstand basierend auf der Norm
-        VDI 2055-1 [2019] für die thermischen Verluste gedämmter Rohrleitungen
-        verlegt in Registeranordnung in ein- oder mehrschichtigen Platten
-        (z. B. Fußbodenheizungen) - vereinfacht und angepasst in [Apfel2020]
-        
-        - variable Größen:
-            - minimaler Oberflächenabstand x_min [m]
-            - Wärmeleitfähigkeiten Beton und Kondensatrohre [W/mK]
-            - Rohrgeometrie des Kondensator (Durchmesser, Rohrabstand,
-                                             Rohrlänge) [m]
+    - analytisch ermittelter therm. Widerstand basierend auf der Norm
+    VDI 2055-1 [2019] für die thermischen Verluste gedämmter Rohrleitungen
+    verlegt in Registeranordnung in ein- oder mehrschichtigen Platten
+    (z. B. Fußbodenheizungen) - vereinfacht und angepasst in [Apfel2020]
+    
+    - variable Größen:
+        - minimaler Oberflächenabstand x_min [m]
+        - Wärmeleitfähigkeiten Beton und Kondensatrohre [W/mK]
+        - Rohrgeometrie des Kondensator (Durchmesser, Rohrabstand,
+                                         Rohrlänge) [m]
 
     Autor: Yannick Apfel
 """
@@ -32,6 +32,7 @@ def R_th_he(he):  # thermischer Widerstand [K/W]
     # 3.) thermischer Widerstand des Heizelements in [K/W] --> R_th = 1 K / (q_l * l_R)
     ''' andere Seite wird als thermisch isoliert betrachtet: state_u_insul=True
     '''
-    R_th_he = (he.l_R * q_l(x_o, x_u, he.d_R_a, he.d_R_i, he.lambda_B, he.lambda_R, he.s_R, Theta_R, Theta_inf_o, state_u_insul=True)) ** -1
+    R_th_he = (he.l_R * q_l(x_o, x_u, he.d_R_a, he.d_R_i, he.lambda_B, he.lambda_R, he.s_R, Theta_R, Theta_inf_o, 
+                            state_u_insul=True)) ** -1
 
     return R_th_he
